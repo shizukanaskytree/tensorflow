a1f97193016813cea98a6af3f8472b7b391f4c4d
shizukanaskytree <shizukanaskytree@gmail.com>
2019-05-07 01:25:29 -0500

    update run_build.sh

--- run_build.sh
+++ run_build.sh
@@ -5,6 +5,6 @@ bazel build --config=opt \
             //tensorflow/tools/pip_package:build_pip_package &&
 # install package
 /home/wxf/tf2/tensorflow/bazel-bin/tensorflow/tools/pip_package/build_pip_package --nightly_flag /tmp/tensorflow_pkg &&
-pip3 uninstall --yes /tmp/tensorflow_pkg/tf_nightly-1.13.1-cp36-cp36m-linux_x86_64.whl &&
-pip3 install /tmp/tensorflow_pkg/tf_nightly-1.13.1-cp36-cp36m-linux_x86_64.whl
+/home/wxf/anaconda3/bin/pip3 uninstall --yes /tmp/tensorflow_pkg/tf_nightly-1.13.1-cp36-cp36m-linux_x86_64.whl &&
+/home/wxf/anaconda3/bin/pip3 install /tmp/tensorflow_pkg/tf_nightly-1.13.1-cp36-cp36m-linux_x86_64.whl
 
