2e6fee50cb0e7b7f83506998a9f19867d5a2a960
shizukanaskytree <shizukanaskytree@gmail.com>
2020-02-08 21:58:50 -0600

    Update build script

--- run_build.sh
+++ run_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-bazel build --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0" //tensorflow/tools/pip_package:build_pip_package &&
+bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package &&
 # install package
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package --nightly_flag /tmp/tensorflow_pkg &&
-pip install /tmp/tensorflow_pkg/tf_nightly-2.0.0-cp36-cp36m-linux_x86_64.whl
+pip uninstall --yes /tmp/tensorflow_pkg/tf_nightly-2.1.0-cp36-cp36m-linux_x86_64.whl &&
+pip install /tmp/tensorflow_pkg/tf_nightly-2.1.0-cp36-cp36m-linux_x86_64.whl
