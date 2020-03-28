3ee222e08aa482cda4e6a4502ed8f4734ba7854d
shizukanaskytree <shizukanaskytree@gmail.com>
2020-03-27 20:27:59 -0500

    Update run_build.sh

--- run_build.sh
+++ run_build.sh
@@ -6,7 +6,7 @@ bazel build --config=opt \
             --config=cuda \
             //tensorflow/tools/pip_package:build_pip_package &&
 # install package
-/home/wxf/tf2/crash_tf/tensorflow/bazel-bin/tensorflow/tools/pip_package/build_pip_package --nightly_flag /tmp/tensorflow_pkg &&
-pip3 uninstall --yes /tmp/tensorflow_pkg/tf_nightly-1.13.1-cp36-cp36m-linux_x86_64.whl &&
-pip3 install /tmp/tensorflow_pkg/tf_nightly-1.13.1-cp36-cp36m-linux_x86_64.whl
+bazel-bin/tensorflow/tools/pip_package/build_pip_package --nightly_flag /tmp/tensorflow_pkg &&
+pip uninstall --yes /tmp/tensorflow_pkg/tf_nightly-1.13.1-cp36-cp36m-linux_x86_64.whl &&
+pip install /tmp/tensorflow_pkg/tf_nightly-1.13.1-cp36-cp36m-linux_x86_64.whl
 
