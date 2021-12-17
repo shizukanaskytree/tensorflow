1df6ad91d0e37c9ce8213b68cb08c7a8ca5960ac
shizukanaskytree <shizukanaskytree@gmail.com>
2021-12-17 14:00:32 -0800

    mnist real data

---
+++ experiments/models/mnist_pytorch_real_data/main.py
@@ -0,0 +1,103 @@
+# https://nextjournal.com/gkoehler/pytorch-mnist
+
+import torch
+import torchvision
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import numpy as np
+
+n_epochs = 3
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
+
+random_seed = 1
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
+
+train_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./data', train=True, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_train, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('./data', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_test, shuffle=True)
+
+
+examples = enumerate(test_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+
+print('example_data.shape: ', example_data.shape) # example_data.shape:  torch.Size([1000, 1, 28, 28])
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()        
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        # input shape
+        print(x.shape)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+network = Net()
+optimizer = optim.SGD(network.parameters(), lr=learning_rate,
+                      momentum=momentum)
+
+
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+
+def train(epoch):
+    network.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()        
+        print('forward-ing')
+        print(f'data.shape: {data.shape}')
+        output = network(data)
+        print('forward-ed')
+        
+        print(f'target.shape: {target.shape}')
+        loss = F.nll_loss(output, target)   
+        loss.backward()        
+        optimizer.step()
+             
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            train_losses.append(loss.item())            
+            train_counter.append(
+                (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+        
+        # torch.save(network.state_dict(), '/results/model.pth')
+        # torch.save(optimizer.state_dict(), '/results/optimizer.pth')
+
+train(epoch=1)
