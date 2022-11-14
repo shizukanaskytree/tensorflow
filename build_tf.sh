3f287b9072dd34a13cc019fee0e0373a97a637da
shizukanaskytree <shizukanaskytree@gmail.com>
2022-11-14 14:02:24 -0600

    build script

---
+++ build_tf.sh
@@ -0,0 +1,9 @@
+# docs: https://www.tensorflow.org/install/source
+# issue: https://github.com/tensorflow/serving/issues/976
+./configure
+bazelisk build --config=cuda //tensorflow/tools/pip_package:build_pip_package
+# ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package --nightly_flag /tmp/tensorflow_pkg
+
+### Install the package
+# pip install /tmp/tensorflow_pkg/tensorflow-
